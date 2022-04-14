fb67cb93a3fee7bd3af42b0099a9b5f9cba6305a
pvscvl <pascalschoofs@icloud.com>
2022-04-14 22:44:01 +0200

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install coreutils ffmpeg iperf iperf3 youtube-dl      
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+brew install ffmpeg 
+brew install iperf 
+brew install iperf3 
+brew install youtube-dl      
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+brew install gmp
+
+brew install zsh-syntax-highlighting
