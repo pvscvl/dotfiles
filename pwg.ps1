2429e60fb6c48159546ab291ed131c10c755feac
pvscvl <pascalschoofs@icloud.com>
2022-05-11 21:35:50 +0200

    ..

--- pwg.ps1
+++ pwg.ps1
@@ -3,13 +3,20 @@ function Get-RandomCharacters($length, $characters) {
     $private:ofs=""
     return [String]$characters[$random]
 }
- 
 
-$password = Get-RandomCharacters -length 1 -characters '!%#_'
-$password += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
-$password += Get-RandomCharacters -length 9 -characters 'abcdefghiklmnoprstuvwxyz'
-$password += Get-RandomCharacters -length 1 -characters '1234567890'
- 
+ function Shuffle-String([string]$inputString){     
+    $characterArray = $inputString.ToCharArray()   
+    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
+    $outputString = -join $scrambledStringArray
+    return $outputString 
+}
 
-Write-Host $password | Set-Clipboard
+$password = Get-RandomCharacters -length 2 -characters '!%#_'
+$password += Get-RandomCharacters -length 3 -characters 'ABCDEFGHKLMNPRSTUVWXYZ'
+$password += Get-RandomCharacters -length 4 -characters 'abcdefghikmnoprstuvwxyz'
+$password += Get-RandomCharacters -length 2 -characters '1234567890'
+ 
+$password = Shuffle-String $password
+echo -n $password | Clip
+Write-Host ""
 Write-Host $password
